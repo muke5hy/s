8e286d5504607353feb87a3e12ea127f3dbefb47
Karthik Praveen <karthikpraveens95@gmail.com>
2017-04-05 19:24:19 +0530

    adding service worker

--- public/javascripts/sController.js
+++ public/javascripts/sController.js
@@ -46,3 +46,16 @@ sApp.directive("selectClick", function () {
         }
     };
 });
+
+if ('serviceWorker' in navigator) {
+
+  navigator.serviceWorker
+    .register('./service-worker.js', { scope: './' })
+    .then(function(registration) {
+      console.log("Service Worker Registered");
+    })
+    .catch(function(err) {
+      console.log("Service Worker Failed to Register", err);
+    })
+
+}
